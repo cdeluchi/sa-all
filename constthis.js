d45538f93bb7fc690ac61faee8b460071aca3c5c
Clarice Deluchi <cdeluchi@gmail.com>
2021-08-13 17:41:44 +0200

    ex2

--- constthis.js
+++ constthis.js
@@ -32,8 +32,20 @@ console.log(square.getArea());
 // ex 2
 
 // Write a function called invertCase that expects a string as a parameter.
-// This function should return a new string with all the same characters as the string that was passed in
-// but with the cases of the alphabetic characters switched.
 // Uppercase characters should become lowercase and lowercase letters should become uppercase.
 // Characters that are not alphabetic should not change.
 // The toUpperCase and toLowerCase methods that all strings have will come in handy here.
+
+function invertCase(str) {
+    var switchUp = "";
+    var switchDown = "";
+
+    // used loop but the loop do not return the up to down
+
+    for (var i = 0; i < str.length; i++) {}
+    }
+    // This function should return a new string with all the same characters as the string that was passed in
+    // but with the cases of the alphabetic characters switched.
+    return switchDown;
+}
+console.log(invertCase("FRIDAY night!13"));
