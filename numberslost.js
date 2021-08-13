4adaebd1235f50573d9ea99b70688f9929c7d299
Clarice Deluchi <cdeluchi@gmail.com>
2021-08-13 13:53:11 +0200

    first try

---
+++ numberslost.js
@@ -0,0 +1,10 @@
+var x = 25;
+var doublex = [];
+
+function timesTwo(number) {
+    return number * 2;
+    for (var i = 0; i < number; i++);
+    number[{ y: doublex }];
+}
+var numbers = [x, doublex];
+timesTwo(x[doublex]);
