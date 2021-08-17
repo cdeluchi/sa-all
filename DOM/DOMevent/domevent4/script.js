f62928ae58f8e17aa336319ed1b6aca6b471d5c5
Clarice Deluchi <cdeluchi@gmail.com>
2021-08-17 21:08:02 +0200

    created the first part

--- DOM/DOMevent/domevent4/script.js
+++ DOM/DOMevent/domevent4/script.js
@@ -1,9 +1,44 @@
 // DOM event 4
 
-// Make a page that has on it an element that is 200px by 200px in size and has a solid background color.
-// Nest within that element another element that is 50px by 50px in size and has a different solid background color.
 // When the user clicks on the outer element its background color should change to a randomly selected color.
 // However, if the user clicks on the inner element, the inner element's background color should change to
 // a randomly selected background color but the outer element's background color should not change at all.
 
-console.log("Iam here!");
+// console.log("Iam here!");
+
+// create 2 bags with my boxs (var) boxOutside and (var) boxInside
+// function has to contain the color generate randonly
+// continue to use the Math.floor and Math.random to criate the numbers for my color rgb
+
+var boxOutside = document.getElementById("boxoutside");
+var boxInside = document.getElementById("boxinside");
+console.log("I am right?");
+function generateRandomColorNum() {
+    return Math.floor(Math.random() * 256);
+}
+var generateRandomColor = () => {
+    var r = generateRandomColorNum();
+    var g = generateRandomColorNum();
+    var b = generateRandomColorNum();
+    var randomColor = "rgb(" + r + "," + g + "," + b + ")";
+    return randomColor;
+};
+console.log("and now?");
+boxOutside.addEventListener("click", function () {
+    boxOutside.style.backgroundColor = generateRandomColor();
+});
+
+boxInside.addEventListener("click", function () {
+    boxInside.style.backgroundColor = generateRandomColor();
+});
+
+// has to stop the "outsideBox" and changes "insideBox"
+// need to use stop bubbling or stop propagation
+
+var boxoutside = document.getElementById("boxoutside");
+boxoutside.addEventListener("click", function (event) {
+    event.stopPropagation();
+    console.log("it's right?");
+    boxinside += generateRandomNum();
+    color.style.left = boxinside + "px";
+});
