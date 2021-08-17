0f2c56e332f37e4b29324fe92ea0c4b04aea9f0e
Clarice Deluchi <cdeluchi@gmail.com>
2021-08-17 15:33:07 +0200

    start

---
+++ DOM/DOMevent/script.js
@@ -0,0 +1,19 @@
+// DOM event 1
+
+// Make a page that has on it an element that is 100px by 100px in size,
+// has absolute positioning, and has a solid background color.
+// Add an event handler that makes this box center itself directly under the user's
+// mouse pointer as it is moved across the screen.
+
+// console.log("i am here");
+
+
+function (){
+     var moveTheSquare = document.getElementById("event");
+     for (var i = 0; i<)
+}
+
+// Make a page that has on it an element that is 100px by 100px in size and has a solid black border. 
+// When the user mouses down on this box, its background should change to a randomly selected color. 
+// When the user mouses up on it, its background should change to another randomly selected color.
+
