9edf7733342d012f13a75a3b37c3ae23dfa4ad9f
Clarice Deluchi <cdeluchi@gmail.com>
2021-08-18 20:41:11 +0200

    new chages

--- webpage/script.js
+++ webpage/script.js
@@ -1,13 +1,23 @@
 console.log("menu");
-var menu = document.querySelector("menu");
 var showMenu = document.querySelector(".overlay");
 var hideMenu = document.querySelector(".side-nav");
 var X = document.getElementsById("closeButton");
 
-showMenu.addEventListener("click", function () {
-    menu.classList.add("on");
-});
+function sideMenu() {
+    showMenu.addEventListener("click", function () {
+        menu.classList.add("on");
+    });
 
-hideMenu.addEventListener("click", function () {
-    menu.classList.add("on");
+    hideMenu.addEventListener("click", function () {
+        menu.classList.remove("hidden");
+    });
+}
+
+var sideBarMenu = document.getElementsById("menu");
+sideBarMenu.addEventListener("click", function () {
+    sideNav.classList.add("on");
+    sideNav.classList.remove("hidden");
+    overlay.classList.remove("hidden");
 });
+X.addEventListener("click");
+overlay.addEventListener("click");
