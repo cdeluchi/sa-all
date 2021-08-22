a360339fa5478c522db8af5ec73b81111a438ce8
Clarice Deluchi <cdeluchi@gmail.com>
2021-08-22 20:13:48 +0200

    final

--- canvas/script.js
+++ canvas/script.js
@@ -1,92 +1,94 @@
-// console.log("I am heree!");
+console.log("here");
+// function() {
+var context = document.getElementById("canv").getContext("2d");
 
-(function () {
-    var draw1 = document.getElementById("canvas").getContext("2d");
+// body
+context.beginPath();
+context.strokeStyle = "#EF476F";
+context.moveTo(100, 50);
+context.lineTo(100, 150);
+context.stroke();
 
-    // head
-    draw1.beginPath();
-    draw1.strokeStyle = "#c400ff";
-    draw1.lineWidth = 10;
-    draw1.fillStyle = "#ff67e7";
-    draw1.arc(200, 150, 70, 0, 2 * Math.PI);
-    draw1.stroke();
-    draw1.fill();
+// arms
+context.beginPath();
+context.strokeStyle = "#26547C";
+context.moveTo(100, 50);
+context.lineTo(150, 100);
+context.moveTo(100, 50);
+context.lineTo(50, 100);
+context.moveTo(0, 0);
+context.stroke();
 
-    // body
-    draw1.beginPath();
-    draw1.strokeStyle = "#EF476F";
-    draw1.moveTo(300, 300);
-    draw1.lineTo(300, 550);
-    draw1.stroke();
+// legs
+context.beginPath();
+context.strokeStyle = "#06D6A0";
+// context.moveTo(200, 150);
+// context.lineTo(350, 200);
+// context.moveTo(80, 180);
+// context.lineTo(100, 150);
+// context.moveTo(200, 360);
+// context.lineTo(100, 150);
+context.moveTo(100, 150);
+context.lineTo(50, 200);
+context.moveTo(100, 150);
+context.lineTo(150, 200);
+context.stroke();
 
-    // right arm
-    draw1.beginPath();
-    draw1.moveTo(300, 300);
-    draw1.lineTo(400, 400);
-    draw1.strokeStyle = "#26547C";
-    draw1.stroke();
+// head
+context.beginPath();
+context.strokeStyle = "#c400ff";
+context.lineWidth = 20;
+context.fillStyle = "#ff67e7";
+context.arc(100, 30, 10, 0, 2 * Math.PI);
+context.stroke();
+// context.fill();
 
-    // left arm
-    draw1.beginPath();
-    draw1.moveTo(300, 300);
-    draw1.lineTo(200, 400);
-    draw1.strokeStyle = "#26547C";
-    draw1.stroke();
-
-    // right leg
-    draw1.beginPath();
-    draw1.moveTo(300, 300);
-    draw1.lineTo(400, 400);
-    draw1.strokeStyle = "#06D6A0";
-    draw1.stroke();
-
-    // left leg
-    draw1.beginPath();
-    draw1.moveTo(500, 500);
-    draw1.lineTo(400, 400);
-    draw1.strokeStyle = "#06D6A0";
-    draw1.stroke();
-})();
+// }
 
 // (function () {
-//     var draw1 = document.getElementsById("canvas")[0];
-//     var draw2 = can.getContext("2d");
+//     var draw1 = document.getElementById("canvas").getContext("2d");
+
+//     // head
+//     draw1.beginPath();
+//     draw1.strokeStyle = "#c400ff";
+//     draw1.lineWidth = 10;
+//     draw1.fillStyle = "#ff67e7";
+//     draw1.arc(200, 150, 70, 0, 2 * Math.PI);
+//     draw1.stroke();
+//     draw1.fill();
 
 //     // body
-//     ctx.beginPath();
-//     ctx.moveTo(100, 50);
-//     ctx.lineTo(100, 200);
-//     ctx.stroke();
+//     draw1.beginPath();
+//     draw1.strokeStyle = "#EF476F";
+//     draw1.moveTo(300, 300);
+//     draw1.lineTo(300, 550);
+//     draw1.stroke();
 
-//     ctx.beginPath();
-// ctx.lineWidth = 10;
-//     ctx.strokeStyle = "saddlebrown";
-//     ctx.moveTo(100, 200);
-//     ctx.lineTo(200, 125);
-//     ctx.stroke();
-// });
+//     // right arm
+//     draw1.beginPath();
+//     draw1.moveTo(300, 300);
+//     draw1.lineTo(400, 400);
+//     draw1.strokeStyle = "#26547C";
+//     draw1.stroke();
 
-// can.addEventListener("dblclick", function () {
-//     ctx.clearRect(0, 0, can.width / 2, can.height / 2);
-// });
+//     // left arm
+//     draw1.beginPath();
+//     draw1.moveTo(300, 300);
+//     draw1.lineTo(200, 400);
+//     draw1.strokeStyle = "#26547C";
+//     draw1.stroke();
 
-//     ctx.beginPath();
-//     ctx.lineWidth = 20;
-//     ctx.strokeStyle = "limegreen";
-//     ctx.moveTo(200, 125);
-//     ctx.lineTo(100, 50);
-//     ctx.stroke();
+//     // right leg
+//     draw1.beginPath();
+//     draw1.moveTo(300, 300);
+//     draw1.lineTo(400, 400);
+//     draw1.strokeStyle = "#06D6A0";
+//     draw1.stroke();
 
-//     ctx.fillStyle = "papayawhip";
-//     ctx.fillRect(250, 300, 350, 400);
-
-//     ctx.strokeRect(210, 260, 370, 380);
-
-//     ctx.beginPath();
-//     ctx.strokeStyle = "midnightblue";
-//     ctx.fillStyle = "olivedrab";
-//     ctx.arc(200, 200, 50, 0, 2 * Math.PI);
-//     ctx.stroke();
-//     ctx.fill();
-
+//     // left leg
+//     draw1.beginPath();
+//     draw1.moveTo(500, 500);
+//     draw1.lineTo(400, 400);
+//     draw1.strokeStyle = "#06D6A0";
+//     draw1.stroke();
 // })();
