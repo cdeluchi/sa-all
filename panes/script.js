f816798de221003acde7313fabf740221580322d
Clarice Deluchi <cdeluchi@gmail.com>
2021-08-23 16:56:32 +0200

    add some jQuery

--- panes/script.js
+++ panes/script.js
@@ -1,9 +1,9 @@
 console.log("jQuery linked?", $);
 
-var document = $(document);
+var document = $(.container);
 var bottom = $(".bottom");
-var top = $(".top").outerWidth();
-var slider = $(".slider").offset().left;
+var top = $(".top");
+var slider = $(".slider");
 
 $(function () {
     $(document).resizable({
@@ -32,30 +32,3 @@ slider.on("mousedown", function (e) {
     });
 });
 
-// var document = $(document);
-// var slider = $(".slider");
-// var widthSize = $(".top").outerWidth();
-// var rightLimit = $(".slider").offset().left;
-
-// var left = 0;
-
-// console.log(widthSize, rightLimit);
-
-// slider.on("mousedown", function (e) {
-//     e.preventDefault();
-
-//     $(document).on("mousemove", function (e) {
-//         slider.css({
-//             left: e.clientX + "px",
-//         });
-
-//         $(".top").css({
-//             width: e.clientX + "px",
-//         });
-//     });
-
-//     $(document).on("mouseup", function (e) {
-//         console.log("Not moving");
-//         $(document).unbind();
-//     });
-// });
