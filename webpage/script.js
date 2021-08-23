1305a3daabcd3377853c6f73bd4e636233cd6630
Clarice Deluchi <cdeluchi@gmail.com>
2021-08-23 17:43:21 +0200

    add jQuery

--- webpage/script.js
+++ webpage/script.js
@@ -1,45 +1,37 @@
 // console.log("menu");
-var menu = document.querySelector(".menu");
-var showMenu = document.querySelector(".overlay");
-var hideMenu = document.querySelector(".side-nav");
-var x = document.getElementById("closeButton");
+// var menu = document.querySelector(".menu");
+// var showMenu = document.querySelector(".overlay");
+// var hideMenu = document.querySelector(".side-nav");
+// var x = document.getElementById("closeButton");
 
-menu.addEventListener("click", function () {
-    menu.classList.add("on");
-});
-menu.addEventListener("click", function () {
-    menu.classList.add("hidden");
-});
+// menu.addEventListener("click", function () {
+//     menu.classList.add("on");
+// });
+// menu.addEventListener("click", function () {
+//     menu.classList.add("hidden");
+// });
 
-var sideBarMenu = document.getElementById("menu");
-sideBarMenu.addEventListener("click", function () {
-    hideMenu.classList.add("on");
-    hideMenu.classList.remove("hidden");
-    showMenu.classList.remove("hidden");
-});
-x.addEventListener("click");
-showMenu.addEventListener("click");
+// var sideBarMenu = document.getElementById("menu");
+// sideBarMenu.addEventListener("click", function () {
+//     hideMenu.classList.add("on");
+//     hideMenu.classList.remove("hidden");
+//     showMenu.classList.remove("hidden");
+// });
+// x.addEventListener("click");
+// showMenu.addEventListener("click");
 
 // pop Up hover
-
-$(window).load(function () {
-    $(".clickpopup").click(function () {
-        $(".hoverpopup").show();
-    });
-    $(".hoverpopup").click(function () {
-        $(".hoverpopup").hide();
-    });
-    $(".closepopup").click(function () {
-        $(".hoverpopup").hide();
-    });
+// hidding our banner
+$(".hoverpopup").hide();
+// make the banner apper after 1s
+setTimeout(function () {
+    console.log("code is runnig after 1s");
+    $(".hoverpopup").show();
+}, 1000);
+$(".x").on("click", function () {
+    console.log("click on the x");
+    $(".hoverpopup").hide();
 });
+console.log($(".x"));
 
-$(function clickbutton(e) {
-    $(".popup").on("click", function () {
-        $(".popup, .hoverpopup").addClass("active");
-    });
-});
-
-$(".close, .popup").on("click", function () {
-    $(".popup, .hoverpopup").removeClass("active");
-});
+console.log($(".hoverpopup"));
