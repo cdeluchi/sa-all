5dd26253466e5ae0b0a6afa8a5511c5bebd6ea92
Clarice Deluchi <cdeluchi@gmail.com>
2021-08-25 16:49:21 +0200

    my string are empty yet

--- local-storage/script.js
+++ local-storage/script.js
@@ -1,15 +1,14 @@
 console.log("local Storage", $);
 
 (function () {
-    localStorage.setItem("text", "");
-    console.log(localStorage.getItem("text")); // logs "Failing  to prepare is preparing to fail."
-    localStorage.removeItem("text");
-
-    try {
-        localStorage.setItem("text", "");
-        console.log(localStorage.getItem("text")); // logs "Failing  to prepare is preparing to fail."
-        localStorage.removeItem("text");
-    } catch (e) {
-        console.log("text");
-    }
-});
+    var str = [];
+    // created an ampty string, who will receive all the text/info that was pass in a text box
+    // function to give the text to the ampty str.
+    $("textarea").val(localStorage.getItem("text"));
+    $("textarea").on("input", function () {
+        //    that text will return to the empty str []
+        return str;
+    });
+    // will set the info, when the user text something, will fixed here
+    localStorage.setItem("text", $("textarea").val());
+})();
