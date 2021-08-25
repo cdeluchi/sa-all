efba1c275f8894df40426a79a127ad05f4c56455
Clarice Deluchi <cdeluchi@gmail.com>
2021-08-25 21:22:39 +0200

    final

--- local-storage/script.js
+++ local-storage/script.js
@@ -1,14 +1,11 @@
 console.log("local Storage", $);
 
 (function () {
-    var str = [];
     // created an ampty string, who will receive all the text/info that was pass in a text box
     // function to give the text to the ampty str.
     $("textarea").val(localStorage.getItem("text"));
     $("textarea").on("input", function () {
         //    that text will return to the empty str []
-        return str;
+        localStorage.setItem("text", $("textarea").val());
     });
-    // will set the info, when the user text something, will fixed here
-    localStorage.setItem("text", $("textarea").val());
 })();
