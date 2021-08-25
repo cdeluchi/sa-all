e6a7299b87270d08d42a31de85f4aa1b9c15bff9
Clarice Deluchi <cdeluchi@gmail.com>
2021-08-25 13:14:58 +0200

    created file

---
+++ translate-number/script.js
@@ -0,0 +1,24 @@
+console.log("translate Number", $);
+(function () {
+    function askForNumber() {
+        var num = prompt("Please enter a number between 1 and 10");
+        if (num >= 1 && num <= 10 && num == parseInt(num)) {
+            return num;
+        }
+        throw new Error("Bad number");
+    }
+    function translateNumberToGerman() {
+        var nummerDeutch = [
+            "eins",
+            "zwei",
+            "drei",
+            "vier",
+            "fünf",
+            "sechs",
+            "sieben",
+            "acht",
+            "neun",
+            "zehn",
+        ];
+    }
+});
