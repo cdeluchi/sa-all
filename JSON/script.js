874e30c7d969823b32ff47b9411c023071ae5f05
Clarice Deluchi <cdeluchi@gmail.com>
2021-08-26 13:13:44 +0200

    started the changes

--- JSON/script.js
+++ JSON/script.js
@@ -1,40 +1,48 @@
 console.log("the JSON", $);
 
-(function () {
-    $("button").on("click", function () {
-        try {
-            JSON.parse($("textbox").val());
-            alert("this is valid JSON");
-            // try { // try to parse your input value // IF we make it to this line, we can alert("this is valid JSON") }
-            // catch (e){ // IF we land in the catch block that means what the user input could not be parsed
-            // so we need to alert("this is not JSON") }
-        } catch (err) {
-            alert("this is not JSON");
-            // console.log(err);
-        }
-    })();
-
-    //     try {
-    //     asdfasfasf;
-    // } catch (e) {
-    //     console.log(e); //logs 'ReferenceError: asdfasfasf is not defined'
-    // }
-    // console.log(typeof e); //logs 'undefined'
-
+$("button").on("click", function () {
     // catchText.on("input", function () {
-    //     var inputVal = catchText.val();
-    //     // console.log(catchText.val());
-    //     var realJson = [];
-    //     button.on("click", function () {
-    //         if (inputVal === parseInt) {
-    //             // console.log("inputVal");
-    //             return "Yes, this is a JSON!";
-    //         } else {
-    //             return "no, you're wrong!";
-    //         }
-    //     });
-    // });
+    var inputVal = $("textarea").val();
+    // console.log(catchText.val());
+    // button.on("click", function () {
+    try {
+        // var realJson = []; can not acepted an empty []
+        var realJson = JSON.parse(inputVal);
+        alert("this is valid JSON", realJson);
+    } catch (erro) {
+        alert("this is not JSON", erro);
+    }
 });
+
+// if (inputVal === parseInt) {
+//         // console.log("inputVal");
+//         return "Yes, this is a JSON!";
+//     } else {
+//         return "no, you're wrong!";
+//     }
+// });
+// });
+
+// $("button").on("click", function () {
+//     var
+//     console.log("this button works?", "click");
+//     try {
+//         JSON.parse($("textvalidation").val());
+//         window.alert("this is valid JSON");
+//         // try { // try to parse your input value // IF we make it to this line, we can alert("this is valid JSON") }
+//         // catch (e){ // IF we land in the catch block that means what the user input could not be parsed
+//         // so we need to alert("this is not JSON") }
+//     } catch (err) {
+//         window.alert("this is not JSON");
+//         console.log("this is not an err", err);
+//     }
+
+//     try {
+//     asdfasfasf;
+// } catch (e) {
+//     console.log(e); //logs 'ReferenceError: asdfasfasf is not defined'
+// }
+// console.log(typeof e); //logs 'undefined'
 
 // upon clicking we want to check if we can actually parse with the help of JSON.parse(),
 // what the user put in, that would be our correct check for whether or not sth is valid JSON;
--- ticker/script.js
+++ ticker/script.js
@@ -1,24 +1,69 @@
 (function () {
-    // var headlines = document.getElementById("headlines"); Vanilla JS
-    var jQHeadlines = $("#headlines"); //jQuery
-    // var left = headlines.offsetLeft; Vanilla JS
-    var jQLeft = $("#headlines");
-
-    /* function moveHeadlines() {
-        left--;
-        setTimeout(moveHeadlines, 16.666);
-    }
-    moveHeadlines(); */
+    $.ajax({
+        method: "GET",
+        url: "data.json",
+        success: function (arrayOfHeadlines) {
+            console.log(arrayOfHeadlines);
+            for (var i = 0; i < arrayOfHeadlines.length; i++){
+                html +=
+                $("body").html("<div id="ticker"> " +"<div id="headlines"> " + data.join(", ")  + "</div>" + "</div>");
+            }
+            moveHeadlines();
+        },
+    });
+    var headlines = document.getElementById("headlines");
+    var a = headlines.getElementsByTagName("a");
+    var left = headlines.offsetLeft;
+    var myReqId;
 
     function moveHeadlines() {
         left--;
-        if (left <= -a[0].offsetWidth) {
-            left += a[0].offsetWidth;
+        if (-left === a[0].offsetWidth) {
             headlines.appendChild(a[0]);
+            left = 0;
         }
-        $("#headlines").style.left = `${left}px`;
-        console.log(left);
-        requestAnimationFrame(moveHeadlines);
+        headlines.style.left = left + "px";
+        myReqId = requestAnimationFrame(moveHeadlines);
     }
     moveHeadlines();
+
+    function stopHeadlines(myReqId) {
+        window.cancelAnimationFrame(myReqId);
+    }
+
+    for (var i = 0; i < a.length; i++) {
+        a[i].addEventListener("mouseenter", function () {
+            this.classList.add("hover");
+            stopHeadlines(myReqId);
+        });
+        a[i].addEventListener("mouseleave", function () {
+            this.classList.remove("hover");
+            moveHeadlines();
+        });
+    }
 })();
+
+// (function () {
+//     // var headlines = document.getElementById("headlines"); Vanilla JS
+//     var jQHeadlines = $("#headlines"); //jQuery
+//     // var left = headlines.offsetLeft; Vanilla JS
+//     var jQLeft = $("#headlines");
+
+//     /* function moveHeadlines() {
+//         left--;
+//         setTimeout(moveHeadlines, 16.666);
+//     }
+//     moveHeadlines(); */
+
+//     function moveHeadlines() {
+//         left--;
+//         if (left <= -a[0].offsetWidth) {
+//             left += a[0].offsetWidth;
+//             headlines.appendChild(a[0]);
+//         }
+//         $("#headlines").style.left = `${left}px`;
+//         console.log(left);
+//         requestAnimationFrame(moveHeadlines);
+//     }
+//     moveHeadlines();
+// })();
