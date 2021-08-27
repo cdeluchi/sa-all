086c55a5edfc9a6235e48b0de75d956f7af65f4b
Clarice Deluchi <cdeluchi@gmail.com>
2021-08-27 21:29:38 +0200

    add some JS

--- connect-four/script.js
+++ connect-four/script.js
@@ -1,33 +1,129 @@
 (function () {
-    var playerOne = "player1";
-    var slots = $(".slot");
-    // console.log("how many slots: ", slots);
-    // every slots appear here!
+    var currentPlayer = "player1";
 
-    $(".column").on("click", function (event) {
-        var eachColumn = $(event.currentTarget);
-        var eachSlotInCol = eachColumn.children();
-        // console.log("eachColumn");
+    var slotsInRow = $(".slot");
+    console.log("all slotsInRow ===> ", slotsInRow);
 
-        // now I need to do an for loop to go to all the columns.
-        for (var i = eachSlotInCol.length - 1; i >= 0; i--);
-        {
-            // console.log("which collumn am I? ", eachSlotInCol[i]);
+    $(".column").on("click", function (e) {
+        // console.log("column is clicked!!!!! ", e);
+        var col = $(e.currentTarget);
+        // console.log("col clicked is ===> ", col);
+        var slotsInCol = col.children();
+        // console.log("slotsInCol: ", slotsInCol);
 
-            // and we need to say what we want to do with this information. if the ficha is with another player, it will be with another player.
-            // if the column is full don't have to change the player but is is empty it will be the next one to play.
+        for (var i = slotsInCol.length - 1; i >= 0; i--) {
+            // console.log("each slot in this column ===> ", slotsInCol[i]);
+
             if (
-                !eachSlotInCol.eq(i).hasClass("player1") &&
-                !eachSlotInCol.eq(i).hasClass("player2")
+                !slotsInCol.eq(i).hasClass("player1") &&
+                !slotsInCol.eq(i).hasClass("player2")
             ) {
-                // the eachSlotInCol will call the current player or the player who has the ficha and will play.
-                eachSlotInCol.eq(i).addClass(playerOne);
+                slotsInCol.eq(i).addClass(currentPlayer);
                 break;
             }
         }
-        console.log("i: ", i);
+
+        // console.log("i: ", i);
+
+        var slotsInRow = $(".row" + i);
+        console.log("slotsInRow in row: ", slotsInRow);
+
+        // let's check here for victories before we switch players
+
+        if (checkForColumnVictory(slotsInCol)) {
+            displayWinner();
+            $(".column").trigger("reset");
+        } else if (checkForRowVictory(slotsInRow)) {
+            displayWinner();
+        } else if (checkForDiagonalVictory(slotsInCol, slotsInRow)) {
+            displayWinner();
+        }
         if (i === -1) {
+            // console.log("column is full - dont switch players!!!");
             return;
         }
     });
+    //     // if this is truthy, then we're going to do a victory dance!
+    //     console.log("you have a column victory!!!!!");
+    // } else if (check for horizontal victory) {
+    //     console.log('you have a horizontal victory!!!');
+    // }
+    switchPlayer();
+
+    function switchPlayer() {
+        if (currentPlayer === "player1") {
+            currentPlayer = "player2";
+        } else {
+            // we get in here if the currentplayer is player2
+            currentPlayer = "player1";
+        }
+        // console.log("nextplayer is ====> ", currentPlayer);
+
+        // instead of an if/else, you can also use ternary operator like so:
+        // currentPlayer === "player1"
+        //     ? (currentPlayer = "player2")
+        //     : (currentPlayer = "player1");
+    }
+
+    function displayWinner() {
+        $(".column").off("click");
+        $("h3").css("visibility", "visible");
+        $("#current-player").html(currentPlayer);
+    }
+
+    //  COLUMN VICTORY
+
+    function checkForColumnVictory(slots) {
+        var count = 0;
+        // we're going to write some logic here to check to see who won!
+        // this function expects us to pass it a set of slotsInRow so it can do its check!
+        for (var i = 0; i < slotsInRow.length; i++) {
+            // console.log(slotsInRow.eq(i).hasClass(currentPlayer));
+            if (slotsInRow.eq(i).hasClass(currentPlayer)) {
+                count++;
+                if (count === 4) {
+                    return true;
+                }
+            } else {
+                count = 0;
+            }
+        }
+
+        // console.log("count outside loop: ", count);
+    }
+
+    //  ROW VICTORY
+
+    function checkForRowVictory(slotsInRow) {
+        var count = 0;
+        // we're going to write some logic here to check to see who won!
+        // this function expects us to pass it a set of slotsInRow so it can do its check!
+        for (var i = 0; i < slotsInRow.length; i++) {
+            // console.log(slotsInRow.eq(i).hasClass(currentPlayer));
+            if (slotsInRow.eq(i).hasClass(currentPlayer)) {
+                count++;
+                if (count === 4) {
+                    return true;
+                }
+            } else {
+                count = 0;
+            }
+        }
+    }
+
+    //  DIAGONA VICTORY
+
+    function checkForDiagonalVictory(slotsInCol, slotsInRow) {
+        var count = 0;
+        // we're going to write some logic here to check to see who won!
+        // this function expects us to pass it a set of slotsInRow so it can do its check!
+        for (var i = 0; i < slotsInRow.length; i++) {
+            // console.log(slotsInRow.eq(i).hasClass(playerTurn));
+            if (slotsInRow.eq(i).hasClass(currentPlayer)) {
+                count++;
+                for (var j = 0; j < slotsInRow.length; j++) {}
+                for (var k = 0; k < slots.length; k++) {}
+            }
+        }
+    }
 })();
