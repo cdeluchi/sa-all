c48acc2460072e26fc25386c6a074dc6e9043910
Clarice Deluchi <cdeluchi@gmail.com>
2021-09-01 21:38:00 +0200

    not done yet

---
+++ countdown/script.js
@@ -0,0 +1,11 @@
+// Write a constructor called Countdown that accepts a single argument -
+// the number of seconds to count down.
+// It should be possible to call the start method of instances of Countdown to initiate the countdown.
+// Once the countdown starts, it should count down to zero starting with the number that was passed to the constructor
+// and logging each number to the console with a one second delay.
+
+var s = 10;
+setTimeout(function Countdown(s) {
+    for (var i = 0; i > 10; i--) console.log(i);
+}, 1000);
+Countdown(s);
