3cb6aca1ceedc1805e9bfe55ce1db449a69807b6
Clarice Deluchi <cdeluchi@gmail.com>
2021-09-02 22:56:42 +0200

    tried to understand

---
+++ node/script.js
@@ -0,0 +1,28 @@
+// const myURL = new URL("http://127.0.0.1:8080/test?a=100&b=200");
+// console.log(myURL.href);
+
+// const myURL = new URL("http://127.0.0.1:8080/test?a=100&b=200");
+// console.log(myURL.protocol);
+
+// import url from "http://127.0.0.1:8080/test?a=100&b=200";
+// const myURL = url.parse("http://127.0.0.1:8080/test?a=100&b=200");
+// console.log(myURL);
+
+// node index.js "http://127.0.0.1:8080/test?a=100&b=200"
+const protocol = ;
+const host = ;
+const hostname = ;
+const port = ;
+const pathname = ;
+const query = ;
+const parameter100 = ;
+const parameter200 = ;
+
+// node index.js "https://spiced.academy/program/full-stack-web-development/"
+const protocol = ;
+const host = ;
+const hostname = ;
+const port = ;
+const pathname = ;
+const query = ;
+
