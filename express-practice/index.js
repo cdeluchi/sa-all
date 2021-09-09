8ad7026de5ce9b482bbf6c9836aea72f38246273
Clarice Deluchi <cdeluchi@gmail.com>
2021-09-09 10:44:00 +0200

    done

--- express-practice/index.js
+++ express-practice/index.js
@@ -5,13 +5,18 @@ const cookieParser = require("cookie-parser");
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static("./projects"));
-
 app.use((req, res, next) => {
+    if (req.url !== "/cookie" && req.cookies.cookieAccepted !== "true") {
+        res.cookie("requrl", `${req.url}`);
+        res.redirect("/cookie");
+    } else {
+        next();
+    }
     // console.log(`A ${req.method} request was made to the ${req.url} route`);
-    // console.log("req.cookies: ", req.cookies);
-    next();
 });
-
+app.get("/", (req, res) => {
+    res.send("<h1>This is home! 🙌</h1>");
+});
 app.get("/cookie", (req, res) => {
     res.send(
         `<h1>Accept cookies? 🎠</h1><h2><form method='POST'> <input type="checkbox" name="subscribe"></div> <button> submit </submit> </form>`
@@ -23,14 +28,12 @@ app.post("/cookie", (req, res) => {
     console.log("subscribe: ", subscribe);
     if (subscribe) {
         res.cookie("cookieAccepted", "true");
-        res.send("<h2>You are the best 🤸</h2>");
+        res.redirect(req.cookies.requrl);
     } else {
-        res.send("<h2>You need to accept the cookies 🥊</h2>");
+        res.send("<h2>It's better if you accept the cookies 🥊</h2>");
         // console.log("res", res);
-        app.get("/", (req, res) => {
-            res.send(__dirname + "/index.html");
-        });
     }
 });
+// res.send(generateOverview + "/project");
 
 app.listen(8080, () => console.log("The server is waiting"));
