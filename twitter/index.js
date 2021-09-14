19af252b68014bec3c4c1d0cedb259bdbf7b50e0
Clarice Deluchi <cdeluchi@gmail.com>
2021-09-14 16:24:15 +0200

    part II

--- twitter/index.js
+++ twitter/index.js
@@ -4,18 +4,22 @@ const { getToken, getTweets, filterTweets } = require("./twitter");
 app.use(express.static("../ticker"));
 
 app.get("/data-server.json", (req, res) => {
-    getToken()
-        .then((bearerToken) => {
-            getTweets(bearerToken)
-                .then((tweets) => {
-                    console.log("request Token", bearerToken);
-                    console.log("get Tweets: ", tweets);
-                    const filteredTweets = filterTweets(tweets);
-                    res.json(filteredTweets);
-                })
-                .catch((err) => console.log("err in get Tweets", err));
-        })
-        .catch((err) => console.log("err in request Token", err));
+    getToken().then((useTweets) => {
+        Promise.all([
+            getTweets(useTweets, "Pink"),
+            getTweets(useTweets, "coldplay"),
+            getTweets(useTweets, "beyonce"),
+        ]).then((results) => {
+            const showResults = [...results[0], ...results[1], ...results[2]];
+            const showTweets = showResults.sort((a, b) => {
+                return new Date(b.created_at) - new Date(a.created_at);
+            });
+            console.log("showTweets", showTweets);
+
+            const filteredTweets = filterTweets(showTweets);
+            res.json(filteredTweets);
+        });
+    });
 });
 
 app.listen(8080, () => console.log("Go already and see the tweets!"));
--- twitter/twitter.js
+++ twitter/twitter.js
@@ -4,18 +4,6 @@ const https = require("https");
 const { resolve } = require("path");
 const { consumerKey, consumerSecret } = require("./secrets");
 
-//  ***** very important **** //
-// sempre temos que colocar o arquivo .json no gitignore por que ali estará todos os nossos dados importantes, como dados de cartão de crédito, chaves de api etc..
-
-//  sobre Api do twitter. primeiro precisamos criar uma chave que o Twitter disponibiliza e teremos que usar esse código toda vez que formos precisar desses dados
-// para configurar essa chave precisamos desses 2 arquivos .js um é o index.js que vai ter a função que receberá todas as informações do Twitter
-//  e neste outro o twitter.js terá as funções em si.
-// primeiro temos o module para criar o token do twitter.
-// neste modulo precisamos de uma função com uma callback function que ela retardará a funções principal de rodar.
-//  dentro dela terão mais 2 outras funçnões esta segunda vai verificar se todos os dados que recebemos do token do twitter está correto, por isso ela precisa rodar depois
-//  esta terceira função vai pegar os 'pedaços' do token que geramos primeiro e vai armazendar esses dados na array vazia que criamos antes.
-// nesta última callback ela precisa receber o argumento null na função e ela vai estar alinhada com a função do index.js que também terá um paramentro como erro, este erro vai ser verificado antes de qualqeur outra coisa, caso tenha o erro já sabemos como resolver.
-
 module.exports.getToken = function () {
     return new Promise(function (resolve, reject) {
         let creds = `${consumerKey}:${consumerSecret}`;
@@ -37,9 +25,6 @@ module.exports.getToken = function () {
             if (resp.statusCode != 200) {
                 rejects(resp.statusCode);
             }
-
-            // aqui criamos o body com a array vazia para armazenar todos os dados que o token teceber
-
             let body = "";
             resp.on("data", (chunk) => {
                 body += chunk;
@@ -51,30 +36,23 @@ module.exports.getToken = function () {
     });
 };
 
-// nesta proxima função vai ser quase a mesma coisa só que a diferença é que estamos GET e nao POST
-//  estamos esperando as informações que neste ponto já temos permição do twitter para acessar.
-// nosso objeto também será diferente, aqui vamos ter os dados para acessar os twitter como o metodo GET,
-// o path também é para receber, e aqui não precisamos mais da autorização acess.token somente o bearerToken
-
-module.exports.getTweets = function (bearerToken) {
+module.exports.getTweets = function (bearerToken, useTweets) {
     return new Promise(function (resolve, reject) {
         const options = {
             method: "GET",
             host: "api.twitter.com",
-            path: "/1.1/statuses/user_timeline.json?screen_name=nytimes&tweet_mode=extended",
+            path: `/1.1/statuses/user_timeline.json?screen_name=${useTweets}&tweet_mode=extended`,
             screen_name: "Twitter",
             headers: {
                 Authorization: `Bearer ${bearerToken}`,
             },
         };
-        // esta parte é basicamente igual com o statuscode 200
         const req = https.request(options, cb);
         req.end("grant_type=client_credentials");
         function cb(resp) {
             if (resp.statusCode != 200) {
                 rejects(resp.statusCode);
             }
-            // o body continua vazio para receber os dados parcionados que o twitter vaimandar
             let body = "";
             resp.on("data", (chunk) => {
                 body += chunk;
@@ -85,36 +63,19 @@ module.exports.getTweets = function (bearerToken) {
         }
     });
 };
-// nesta função nao precisamos de uma callback function por que aqui só estamos enviando os dados para a página que criamos antes
-// criamos uma nova variavel para conter todos os dados. lembrando que esse arquivo est´å lincado com o ticker que criamos antes, então tudo o que
-// colocarmos aqui vai aparecer naquela página
-// criamos então um loop que vai passear por todos os twitters fornecidos e procurar exametente o que precisamos que é um twitter com uma única url e que tenha um texto
-// criamos outra variavel agora com um objeto vazio que vai receber essas infomaçnoe sda url e o texto
-//  precisamos dizer pro loop que se os twitters que ele encontrar for igual a 1, então ele envia isso pro objeto que esta vazio e esperando essa informação
-// esse objeto vazio agora precisa falar o que ele quer, então fazemos o twitter[i].full_text e o twitter[i].entities.urls[0].url que seja enviado para o obj vazio
-// isso será enviado por que criamos a funcão .push pra dentro desse objeto formatedTwitter
-//  e no final return a array vazia que agora contem todas as informaões encontradas
 
 module.exports.filterTweets = function (twitter) {
     let twitterData = [];
     for (let i = 0; i < twitter.length; i++) {
         const formatedTweet = {};
         if (twitter[i].entities.urls.length === 1) {
-            console.log("tweets meet condition");
-            // console.log(twitter[i].full_text);
+            // console.log("tweets meet condition");
+            formatedTweet.name = twitter[i].name;
             formatedTweet.text = twitter[i].full_text;
             formatedTweet.url = twitter[i].entities.urls[0].url;
-            // console.log(twitter[i].entities.urls[0].url);
-            // console.log(formatedTweet);
             twitterData.push(formatedTweet);
         }
     }
     console.log(twitterData);
     return twitterData;
 };
-
-// buffer 'base64' vao codificar nosso token vao trazer numeros binários como códigos dificil de acessar
-// nesse objeto vamos armazenar todas as informações que o token precisa. qual é o site, o caminha que ele esta seguindo,
-// metodo nesse caso é o post por que estamos solicitando as informações do twitter. ou seja estamos mandando um requerimento
-// headers será a autorização para uso e o tipo de conteúdo. que estamos usando do let encodedCreds que contem todas as informações do buffer e
-//  resultado dos 2 argumentos que passamos antes que é a chave e a password que nesse caso está sendo buscada no secrets.json que passamos antes
