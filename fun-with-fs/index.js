cf508f7b0d9769e74c1681240a90b125dc9c94df
Clarice Deluchi <cdeluchi@gmail.com>
2021-09-14 17:42:37 +0200

    started

--- fun-with-fs/index.js
+++ fun-with-fs/index.js
@@ -1,71 +1,24 @@
-// **** PART I **** //
-
-// Write a function named logSizes that expects to be passed the full path to a directory.
-// It should use fs.readdir with the withFileTypes option set to true in order to read the contents of the directory.
-
-const fs = require("fs");
+// const fs = require("fs");
 const path = require("path");
 const pathFile = __dirname + "/files";
 
-function logSizes(directory) {
-    fs.readdir(directory, { withFileTypes: true }, (err, files) => {
-        // import { readFile } from "fs";
-        if (err) {
-            return console.log("err: ", err);
-        }
-        for (let i = 0; i < files.length; i++) {
-            if (files[i].isFile()) {
-                fs.stat(directory + "/" + files[i].name, (err, stat) => {
-                    if (err) {
-                        console.log("err", err);
-                    } else {
-                        console.log(directory + "/" + files[i].name, stat.size);
-                    }
-                });
-            } else {
-                const newDirectory = directory + "/" + files[i].name;
-                logSizes(newDirectory);
-            }
-            console.log(
-                "Item " + files[i].name + " is a file:",
-                files[i].isFile()
-            );
-        }
-    });
-} //fii
+// const fs = require("fs");
+const { promisify } = require("util");
+const readdir = promisify(fs.readdir);
+const fs = require("fs").promises;
 
-logSizes(pathFile);
-
-// For every file it should call fs.stat to determine the file's size.
-// Once the size is known, it should log the path to the file followed by a colon
-// and the size of the file.
-
-// For every directory, it should call itself
-// and pass the path to the directory so that the sizes of all the files
-// contained in the directory will be logged.
-
-// **** PART II **** //
-
-function mapSizes(__dirname) {
-    const files = fs.readdirSync(__dirname, { withFileTypes: true });
-    let object = {};
-
-    for (let i = 0; i < files.length; i++) {
-        // console.log("Item " + files[i].name + " is a file:", files[i].isFile());
-        if (files[i].isFile() == true) {
-            const stats = fs.statSync(path.join(__dirname, files[i].name));
-            // console.log(stats);
-            object[files[i].name] = stats.size;
-            console.log(object[files[i].name]);
-        } else {
-            object[files[i].name] = mapSizes;
-            path.join(__dirname, files[i].name);
-        }
-    }
-    return object;
+function logSizes() {
+   return fs.readdir(__dirname).then((files) => console.log(file[0]);
 }
-mapSizes(__dirname);
+logSizes();
 
+
+function mapSizes() {
+    return fs.readdir(__filename).then((files) => console.log(files[0])
+    )}
+    mapSizes();
+
+    
 fs.writeFileSync(
     __dirname + "/README.json",
     JSON.stringify(mapSizes(__dirname), null, 4),
@@ -77,3 +30,8 @@ fs.writeFileSync(
 );
 
 // console.log("Second Important Thing");
+Promise.all([logSizes(directory), mapSizes(__dirname)])
+    .then((results) => {
+        console.log("Done!");
+    })
+    .catch((err) => console.log(err));
