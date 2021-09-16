dcd3b647a3a99fd8e7e316ac6fdd0a9d58590fa8
Clarice Deluchi <cdeluchi@gmail.com>
2021-09-16 13:20:18 +0200

    Table done

--- testing/exercises/3/fn.js
+++ testing/exercises/3/fn.js
@@ -1,26 +1,38 @@
 module.exports = function fn(arg) {
-    function reverseString(arg) {
-        return arg === "" ? "" : reverseString(arg.substr(1)) + arg.charAt(0);
-    }
-    reverseString("Funky Chicken");
-};
-// charAt = A string representing the character (exactly one UTF-16 code unit) at the specified index. If index is out of range, charAt() returns an empty string.
-
-module.exports = function fn(null) {
-    const firstNumber = 90210;
-    function numberNull(firstNumber) {
+    if(typeof arg === String){
+        return reverseString("Funky Chicken");
+    }else if {
+        return null;
+    }if (arg == String){
+        return reverseString("Funky Chicken");
+    }else{
         return null;
     }
-    numberNull();
-};
-
-module.exports = function fn(arg) {
-    function reverseString(arg) {
-        if (90320 === null) {
-            return arg === ""
-                ? ""
-                : reverseString(arg.substr(1)) + arg.charAt(0);
-        }
     }
-    reverseString("Funky Chicken", null);
-};
+
+
+//     function reverseString(arg) {
+//         return arg === "" ? "" : reverseString(arg.substr(1)) + arg.charAt(0);
+//     }
+//     reverseString("Funky Chicken");
+// };
+// // charAt = A string representing the character (exactly one UTF-16 code unit) at the specified index. If index is out of range, charAt() returns an empty string.
+
+// module.exports = function fn(null) {
+//     const firstNumber = 90210;
+//     function numberNull(firstNumber) {
+//         return null;
+//     }
+//     numberNull();
+// };
+
+// module.exports = function fn(arg) {
+//     function reverseString(arg) {
+//         if (90320 === null) {
+//             return arg === ""
+//                 ? ""
+//                 : reverseString(arg.substr(1)) + arg.charAt(0);
+//         }
+//     }
+//     reverseString("Funky Chicken", null);
+// };
